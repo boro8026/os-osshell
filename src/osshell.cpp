c128714f96241660ed8dc338c74818c76586b0c8
Brandon Borowiak <boro8026@stthomas.edu>
2021-03-01 18:11:19 -0600

    my copy

--- src/osshell.cpp
+++ src/osshell.cpp
@@ -6,38 +6,35 @@
 #include <vector>
 #include <unistd.h>
 
-void allocateArrayOfCharArrays(char ***array_ptr, size_t array_length, size_t item_size);
+void splitString(std::string text, char d, std::vector<std::string>& result);
+void vectorOfStringsToArrayOfCharArrays(std::vector<std::string>& list, char ***result);
 void freeArrayOfCharArrays(char **array, size_t array_length);
-void splitString(std::string text, char d, char **result);
+void printHistory(char **history);
 
 int main (int argc, char **argv)
 {
     // Get list of paths to binary executables
-    // `os_path_list` supports up to 16 directories in PATH, 
-    //     each with a directory name length of up to 64 characters
-    char **os_path_list;
-    allocateArrayOfCharArrays(&os_path_list, 16, 64);
+    std::vector<std::string> os_path_list;
     char* os_path = getenv("PATH");
     splitString(os_path, ':', os_path_list);
 
+    ///////////////////////DONT INCLUDE/////////////////////////////////////////////////////
+    //Shows how to loop over the directories in the PATH environment vairable
+    int i;
+    for(i = 0; i < os_path_list.size(); i++)
+    {
+        printf("PATH[%2d]: %s\n", i, os_path_list[i].c_str());
+    }
+    /////////////////////////////////DONT INCLUDE THIS IN FINAL/////////////////////////////
 
-    // Example code for how to loop over NULL terminated list of strings
-    // int i = 0;
-    // while (os_path_list[i] != NULL)
-    // {
-    //     printf("PATH[%2d]: %s\n", i, os_path_list[i]);
-    //     i++;
-    // }
 
 
     // Welcome message
     printf("Welcome to OSShell! Please enter your commands ('exit' to quit).\n");
 
-    // Allocate space for input command lists
-    // `command_list` supports up to 32 command line parameters, 
-    //     each with a parameter string length of up to 128 characters
-    char **command_list;
-    allocateArrayOfCharArrays(&command_list, 32, 128);
+
+    std::vector<std::string> command_list; //to store command user types in, split into its variour parameters
+    char **command_list_exec; //command_list convered to an array of character arrays
 
     // Repeat:
     //  Print prompt for user input: "osshell> " (no newline)
@@ -47,22 +44,27 @@ int main (int argc, char **argv)
     //  For all other commands, check if an executable by that name is in one of the PATH directories
     //   If yes, execute it
     //   If no, print error statement: "<command_name>: Error command not found" (do include newline)
-
     while(true){
         std::cout << "osshell> ";
 
         std::string input;
         std::getline(std::cin, input);
+        
 
         if(input == "exit"){
             // Free allocated memory
-            freeArrayOfCharArrays(os_path_list, 16);
-            freeArrayOfCharArrays(command_list, 32);
+            //freeArrayOfCharArrays(command_list_exec, command_list.size() + 1);
             break;
         }
-
-        if(input == "history"){
-            
+        else if(input == "history"){
+            vectorOfStringsToArrayOfCharArrays(command_list, &command_list_exec);
+            //print the chain of previous commands
+        }
+        else if(input != ""){
+            //split the input from the user
+            //write a method to check if there is an executable by that name in Path directory
+            //if so, execute it
+            //else, print error statement
         }
     }
 
@@ -74,14 +76,17 @@ int main (int argc, char **argv)
    array_length: number of strings to allocate space for in the list
    item_size: length of each string to allocate space for
 */
-void allocateArrayOfCharArrays(char ***array_ptr, size_t array_length, size_t item_size)
+void vectorOfStringsToArrayOfCharArrays(std::vector<std::string>& list, char ***result)
 {
     int i;
-    *array_ptr = new char*[array_length];
-    for (i = 0; i < array_length; i++)
+    int result_length = list.size() + 1;
+    *result = new char*[result_length];
+    for(i = 0; i < list.size();i++)
     {
-        (*array_ptr)[i] = new char[item_size];
+        (*result)[i] = new char[list[i].length() + 1];
+        strcpy((*result)[i], list[i].c_str());
     }
+    (*result)[list.size()] = NULL;
 }
 
 /*
@@ -93,7 +98,10 @@ void freeArrayOfCharArrays(char **array, size_t array_length)
     int i;
     for (i = 0; i < array_length; i++)
     {
-        delete[] array[i];
+        if(array[i] != NULL)
+        {
+            delete[] array[i];
+        }
     }
     delete[] array;
 }
@@ -103,21 +111,59 @@ void freeArrayOfCharArrays(char **array, size_t array_length)
    d: character delimiter to split `text` on
    result: NULL terminated list of strings (char **) - result will be stored here
 */
-void splitString(std::string text, char d, char **result)
+void splitString(std::string text, char d, std::vector<std::string>& result)
 {
-    int i;
-    std::vector<std::string> list;
-    std::stringstream ss(text);
-    std::string token;
-    
-    while (std::getline(ss, token, d))
-    {
-        list.push_back(token);
-    }
+    enum states { NONE, IN_WORD, IN_STRING } state = NONE;
 
-    for (i = 0; i < list.size(); i++)
+    int i;
+    std::string token;
+    result.clear();
+    for(i = 0; i < text.length(); i++)
     {
-        strcpy(result[i], list[i].c_str());
+        char c = text[i];
+        switch(state){
+            case NONE:
+                if(c != d){
+                    if(c == '\"'){
+                        state = IN_STRING;
+                        token = "";
+                    }
+                    else{
+                        state = IN_WORD;
+                        token = c;
+                    }
+                }
+                break;
+            case IN_WORD:
+                if (c == d){
+                    result.push_back(token);
+                    state = NONE;
+                }
+                else{
+                    token += c;
+                }
+                break;
+            case IN_STRING:
+                if (c == '\"'){
+                    result.push_back(token);
+                    state = NONE;
+                }
+                else{
+                    token += c;
+                }
+                break;
+        }
     }
-    result[list.size()] = NULL;
+    if(state != NONE)
+    {
+        result.push_back(token);
+    }
+}
+
+void printHistory(char **history){
+    int i = 0;
+    while(history[i] != NULL){
+        std::cout << "  " << i+1 << ": " << history[i];
+        i++;
+    }
 }
